63ffab7862bd8cddfd1b92d9dcd7be04403da7c3
felipertec <felipe.rtec@gmail.com>
2023-11-17 11:03:40 -0300

    Adicionado imagens e inciar a musica agora quando clica no botao inicia e tambem ao passar o mouse nos quadrados ele mostr ao personagem que pode ser selecionado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,30 @@
-function playAudio(audioName){
-    const audio = new Audio("../src/audio/theme.mp3");
-    audio.volume = 0.5;
-    audio.play();
-}
 let select = document.querySelectorAll('.personagem-item');
 let character = document.getElementById("mostrarPersonagem");
-select.forEach((square) =>{
-    square.addEventListener("mouseenter",()=>{
-        character.src = "../src/images/1.webp"
+let inicio = document.getElementById("acao")
+let header = document.getElementById("header")
+let container = document.getElementById("container")
+
+
+function playAudio(audioName) {
+    let audio = new Audio(`../../src/audio/${audioName}`);
+    audio.volume = 0.2;
+    audio.muted = false;
+    audio.play();
+}
+
+select.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        character.src = `../src/images/${square.id}.webp`
     })
 })
-playAudio();
+
+function iniciar() {
+    playAudio('theme.mp3')
+    inicio.style.display = "none";
+    setTimeout(() => {
+        header.style.display = 'flex';
+        container.style.display = 'flex'
+    }, 1000)
+}
+
+
