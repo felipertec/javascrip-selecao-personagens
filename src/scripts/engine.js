6ec352e794a4f00089cded5a27f75719daf9c2ea
felipertec <felipe.rtec@gmail.com>
2023-11-17 19:55:16 -0300

    add sons ao escolher o personagem e tentado fazer a escolha do personagem

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,6 +3,7 @@ let character = document.getElementById("mostrarPersonagem");
 let inicio = document.getElementById("acao")
 let header = document.getElementById("header")
 let container = document.getElementById("container")
+let escolhido = 0;
 
 
 function playAudio(audioName) {
@@ -13,18 +14,51 @@ function playAudio(audioName) {
 }
 
 select.forEach((square) => {
+    square.addEventListener("click", () => {
+        if(escolhido == 0){
+            escolhido = 1;
+            playAudio(square.id + ".mp3")
+            square.firstChild.style.filter = "brightness(50%)";
+        }
+    })
+    setInterval(()=>{
+        if(escolhido == 1){
+            reload();
+            square.classList.add('personagem-item-novo')
+            square.classList.remove('personagem-item');
+        }
+    },1)
+})
+
+select.forEach((square) => {
     square.addEventListener("mouseenter", () => {
-        character.src = `../src/images/${square.id}.webp`
+        if(escolhido == 0){
+            character.src = `../src/images/${square.id}.webp`
+        }
     })
 })
 
+function reload(){
+    setTimeout(()=>{
+        escolhido = 0;
+        select.forEach((square)=>{ 
+            square.firstChild.style.filter = "brightness(100%)";
+            square.classList.remove('personagem-item-novo');
+            square.classList.add('personagem-item');
+        })
+        console.log('pronto');
+        console.log(escolhido);
+    },5000)
+}
+
 function iniciar() {
     playAudio('theme.mp3')
+    playAudio('haha.mp3')
     inicio.style.display = "none";
     setTimeout(() => {
         header.style.display = 'flex';
         container.style.display = 'flex'
-    }, 1000)
+    }, 2000)
 }
 
 
